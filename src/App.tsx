a561fe86f3b3c058a4cbe49c0c11cd380557aea2
YuriiVasylyshyn <vasylyshyn.yura.19@gmail.com>
2021-03-16 16:12:24 +0200

    feat(router): add navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
+import LoginPage from './pages/login-page/content';
 import SignUpPage from './pages/sign-up-page/content';
 import Header from './components/header/content';
+import { Route, Switch, Redirect } from 'react-router-dom';
+
 
 function App() {
   return (
     <>
       <Header />
-      <SignUpPage />
+      <Redirect from="/" to="/login" />
+      <Switch>
+        <Route
+          path='/login'
+          component={LoginPage}
+        />
+
+        <Route
+          path='/sign-up'
+          component={SignUpPage}
+        />
+      </Switch>
     </>
   );
 }
--- src/pages/login-page/content.tsx
+++ src/pages/login-page/content.tsx
@@ -1,27 +1,24 @@
-import React from "react";
+import React from 'react';
 import Input from '../../components/input/content';
 import styles from '../../styles/sign-up.module.scss';
 import image from '../../assets/images/signup_image.png';
 import { Link } from 'react-router-dom';
 
-class SignUpPage extends React.Component {
+class LoginPage extends React.Component {
     body = <div className={styles.body}>
         <div className={styles.content}>
             <img src={image} alt=''></img>
             <form className={styles.form} action="">
-                <h1 className={styles.title}>Sign Up</h1>
+                <h1 className={styles.title}>Log In</h1>
                 <div className={styles.input}>
                     <span>Email</span>
                     <Input placeHolder='example@mail.com' /></div>
                 <div className={styles.input}>
-                    <span>User Name</span>
-                    <Input placeHolder='alex example...' /></div>
-                <div className={styles.input}>
                     <span>Password</span>
                     <Input placeHolder='Type in...' /></div>
-                <button className={styles.signUpButton}>Sign Up</button>
-                <div className={styles.logIn}>Have an account?
-                    <Link className={styles.logInText} to='/login'>Log In</Link>
+                <button className={styles.signUpButton}>Log in</button>
+                <div className={styles.logIn}>Don`t have an account?
+                <Link className={styles.logInText} to='/sign-up'>Sign Up</Link>
                 </div>
             </form>
         </div>
@@ -33,5 +30,5 @@ class SignUpPage extends React.Component {
 
 }
 
-export default SignUpPage
+export default LoginPage
 
