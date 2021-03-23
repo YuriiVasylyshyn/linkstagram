c5f919df8b60aaf11063c08f106407a6313a4657
YuriiVasylyshyn <vasylyshyn.yura.19@gmail.com>
2021-03-23 18:27:59 +0200

    refactor: add onclick type

--- src/components/button/content.tsx
+++ src/components/button/content.tsx
@@ -3,12 +3,12 @@ import styles from './style.module.scss';
 type ButtonProps = {
   title: string;
   className?: string;
-  onClick?: undefined;
+  onClick?: (e: any) => void;
 };
 
 const Button = ({ title, className, onClick }: ButtonProps) => (
   <button
-    type='button'
+    type="button"
     className={className ?? styles.button}
     onClick={onClick}
   >
