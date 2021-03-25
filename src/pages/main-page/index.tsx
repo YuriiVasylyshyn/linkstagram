56c8ad69cc7af270abb3308177ef22c982e4def6
YuriiVasylyshyn <vasylyshyn.yura.19@gmail.com>
2021-03-25 16:56:34 +0200

    feat: init commit

---
+++ src/pages/main-page/index.tsx
@@ -0,0 +1,7 @@
+import MainLayout from './content';
+
+const MainPage = (): JSX.Element => {
+  return <MainLayout />;
+};
+
+export default MainPage;
--- src/router.tsx
+++ src/router.tsx
@@ -2,13 +2,16 @@ import { Route, Switch } from 'react-router-dom';
 
 import LoginPage from './pages/login-page/content';
 import SignUpPage from './pages/sign-up-page/index';
+import MainPage from './pages/main-page/index';
 
 const Router = () => (
   <>
     <Switch>
       <Route path="/login" component={LoginPage} />
 
-      <Route path="/" component={SignUpPage} />
+      <Route path="/sign-up" component={SignUpPage} />
+
+      <Route path="/" component={MainPage} />
       {/* Add new routes here */}
     </Switch>
   </>
